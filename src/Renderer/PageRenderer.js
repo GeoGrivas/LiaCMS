b3e159ba2a6ef3288546b6005610d79cdc747182
georgios.grivas <giwrgos.grivas5@gmail.com>
2020-11-03 21:35:24 +0200

    logs!

--- src/Renderer/PageRenderer.js
+++ src/Renderer/PageRenderer.js
@@ -13,12 +13,10 @@ class PageRenderer extends React.PureComponent {
     console.log('page 1')
   }
   componentDidUpdate = () => {
+    console.log('page 2');
     if (this.props.currentPage && this.props.currentPage !== this.state.currentPage) {
       axios.get('https://api.adventurouscoding.com/api/pages/' + encodeURIComponent(this.props.currentPage)).then(response => {
         const page = response.data.content;
-        
-        console.log('page2');
-        if (!this.props.loadedLayout)
           this.props.updateLayout(response.data.layout);
         this.loadPage(JSON.parse(page), this.props.currentPage);
       }).catch(err => {
