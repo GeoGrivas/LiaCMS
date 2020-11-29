c94812595a573ad506c83be73f1d126a39cd9871
georgios.grivas <giwrgos.grivas5@gmail.com>
2020-11-29 12:24:15 +0200

    fixed nav logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,15 @@
 import React from 'react';
 //import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
-const logo = (props) => (
-    <div className={classes.Logo} style={{ height: props.height }}>
-        {props.value.startsWith("http") ?
-            <img src={props.value} alt="My Logo" />
-            : <h2>{props.value}</h2>
-        }
-    </div>
-);
+const logo = (props) => {
+    if (props.value.startsWith("http")) {
+        return (
+            <div className={classes.Logo} style={{ height: props.height }}>
+                <img src={props.value} alt="My Logo" />
+            </div>);
+    } else {
+        return (<h2>{props.value}</h2>);
+    }
+};
 
 export default logo;
