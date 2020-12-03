132a20fafcc675b1ee58aa1abf8e5df8e99f846a
georgios.grivas <giwrgos.grivas5@gmail.com>
2020-12-03 17:50:22 +0200

    attempt to fix id issue

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,25 +2,17 @@ import React from 'react';
 import Button from '../UI/Button/Button';
 import classes from './Section.module.css';
 const Section = (props) => {
-    let id = undefined;
-    const idGenerator = (componentName) => {
-        var S4 = function () {
-            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-        return (componentName + S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-    };
-    if (props.block.params?.printable.value == "yes") {
-        id = idGenerator('sct');
-    }
-    const onPrintClicked = (id) => {
-        var prtContent = document.getElementById(id);
+    const id=props.block.id;
+    const onPrintClicked = (sctid) => {
+        const sectionId=sctid;
+        var prtContent = document.getElementById(sectionId);
         var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
         WinPrint.document.write(prtContent.innerHTML);
         WinPrint.document.close();
         WinPrint.focus();
         WinPrint.print();
     }
-    if (id) {
+    if (props.block.params?.printable.value == "yes") {
         return (
             <div>
                 <div style={{
