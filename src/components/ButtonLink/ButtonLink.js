43df47d7df43d2b44733e50be0f0d19915358f87
georgios.grivas <giwrgos.grivas5@gmail.com>
2021-01-14 22:28:36 +0200

    added colour on buttons for pretty printing!

--- src/components/ButtonLink/ButtonLink.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './ButtonLink.module.css';
-
-
-const ButtonLink=(props)=>(
-<a className={classes.ButtonLink} href={props.block.params.url.value}>{props.block.params.text.value}</a>
-);
-
-export default ButtonLink;
